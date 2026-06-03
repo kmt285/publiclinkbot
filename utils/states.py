680802923bd05a8170739ae1b6c3797fb1dc1573
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-06-03 15:09:54 +0630

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -2,4 +2,9 @@ from aiogram.fsm.state import State, StatesGroup
 
 class AdminSetup(StatesGroup):
     waiting_for_payment_info = State()
+    
+    # အောက်ပါ State များကို Service အသစ်ထည့်ရန် ပေါင်းထည့်ထားပါသည်
     waiting_for_service_name = State()
+    waiting_for_service_price = State()
+    waiting_for_service_duration = State()
+    waiting_for_service_link = State()
