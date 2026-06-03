5efdf051eb9825abbdea8ae2493ed8d9d6f224a3
kmt285 <248855908+kmt285@users.noreply.github.com>
2026-06-03 15:20:26 +0630

    Update states.py

--- utils/states.py
+++ utils/states.py
@@ -2,9 +2,10 @@ from aiogram.fsm.state import State, StatesGroup
 
 class AdminSetup(StatesGroup):
     waiting_for_payment_info = State()
-    
-    # အောက်ပါ State များကို Service အသစ်ထည့်ရန် ပေါင်းထည့်ထားပါသည်
     waiting_for_service_name = State()
     waiting_for_service_price = State()
     waiting_for_service_duration = State()
     waiting_for_service_link = State()
+
+class UserBooking(StatesGroup):
+    waiting_for_slip = State() # ဝယ်ယူသူထံမှ Slip ပုံကို စောင့်ဆိုင်းမည့် အခြေအနေ
